1158ca3c4c14c230c55246282e5ca8ea9565cbe7
gitdeepaks <deepaksankhyan92@outlook.com>
2025-05-11 14:21:34 +0530

    added some loops challenges

---
+++ 01-Javascript-concepts/4-forloops/script.js
@@ -0,0 +1,52 @@
+// for Loops
+// for ([initialExpression]; [conditionsExpression]; [incrementExpression]) {
+//   statement;
+// }
+
+// for (let i = 5; i <= 11; i++) {
+//   if (i === 7) {
+//     console.log('7 is my Lucky number');
+//   } else {
+//     console.log('Number' + i);
+//   }
+// }
+
+//Nest Loops
+
+// for (let i = 0; i < 100; i++) {
+//   console.log('Number' + i);
+
+//   for (let j = 0; j < 10; j++) {
+//     console.log(`${i} * ${j} = ${i * j}`);
+//   }
+// }
+
+// Loop through array
+const names = ['sam', 'deepak', 'bala', 'antonio', 'sara'];
+
+for (let i = 0; i < names.length; i++) {
+  if (i === 1) {
+    console.log(names[i] + ' is the best programmer in the world');
+  } else {
+    console.log(names[i]);
+  }
+}
+
+//Break  and continue
+
+// for (let i = 0; i < 20; i++) {
+//   if (i === 15) {
+//     console.log('Breaking');
+//     break;
+//   }
+//   console.log(i);
+// }
+// Continue
+
+for (let i = 0; i < 20; i++) {
+  if (i == 13) {
+    console.log('Skippng 13...');
+    continue;
+  }
+  console.log(i);
+}
