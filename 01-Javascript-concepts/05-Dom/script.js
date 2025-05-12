f9264d522ea9903e3294122c76d5b07ca86a9aee
gitdeepaks <deepaksankhyan92@outlook.com>
2025-05-12 16:27:36 +0530

    DOM intro

---
+++ 01-Javascript-concepts/05-Dom/script.js
@@ -0,0 +1,11 @@
+// console.log(document.links[0]);
+
+// document.body.innerHTML = `<h1>Hello There</h1>`;
+
+// document.writeln('Hello from JS');
+
+console.log(document.getElementById('main'));
+
+const main = document.getElementById('main');
+
+main.innerHTML = '<h1>Hello from main</h1>';
