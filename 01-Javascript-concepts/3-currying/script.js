0cf219559325393fb92e2ab2ae6bbf871f329f53
gitdeepaks <deepaksankhyan92@outlook.com>
2025-05-18 21:29:11 +0530

    learned currying

---
+++ 01-Javascript-concepts/3-currying/script.js
@@ -0,0 +1,73 @@
+// const test = 'string';
+
+// // Currying
+
+// function nam(a) {
+//   return function (b) {
+//     return `${a} ${b}`;
+//   };
+// }
+
+// console.log(nam(5)(6));
+
+// function name1(a) {
+//   return function (b) {
+//     return function (c) {
+//       return `${a} ${b} ${c}`;
+//     };
+//   };
+// }
+
+// console.log(name1(4)(5)(8));
+
+// // evaluate
+
+// function evaluate(oparation) {
+//   return function (a) {
+//     return function (b) {
+//       if (oparation === 'sum') {
+//         return a + b;
+//       } else if (oparation === 'mul') return a * b;
+//       else if (oparation === 'divide') return a / b;
+//       else if (oparation === 'substract') return a - b;
+//       else return 'Invalide oparation';
+//     };
+//   };
+// }
+
+// sum(1)(2); //3
+
+// function sum(a) {
+//   return function (b) {
+//     if (b) return add(a + b);
+//     return a;
+//   };
+// }
+
+// console.log(sum(5)(4)(4)(7)(9)());
+
+// Currying vs Partial Application
+
+// function sum(a) {
+//   return function (b, c) {
+//     return a + b + c;
+//   };
+// }
+
+function curry(fn) {
+  return function curriedFunc(...args) {
+    if (args.length >= fn.length) {
+      return func(...args);
+    } else {
+      return function (...next) {
+        return curriedFunc(...args, ...next);
+      };
+    }
+  };
+}
+
+const sum = (a, b, c) => a + b + c;
+
+const totalSum = curry(sum);
+
+console.log(totalSum(1)(3)(4)(5));
