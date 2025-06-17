890e3e3d1a8b12ccfb94298bda938283e59143fa
gitdeepaks <deepaksankhyan92@outlook.com>
2025-06-17 20:46:43 +0530

    practised  Linked List

---
+++ 01-Javascript-concepts/07-basic-javascript-cllanges/stack-ques-link-list/middleNode.js
@@ -0,0 +1,11 @@
+const middleNode = function (head) {
+  let slowPointer = head;
+  let fastPointer = head;
+
+  while (fastPointer !== null && fastPointer.next !== null) {
+    slowPointer = slowPointer.next;
+    fastPointer = fastPointer.next.next;
+  }
+
+  return slowPointer;
+};
